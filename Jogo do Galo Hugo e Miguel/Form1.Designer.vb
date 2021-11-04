4f6d0d706ec3b210b3ac3898b14b57d3f5ea6d2d
Hugo Gomes <79423600+Hug0mes@users.noreply.github.com>
2021-11-04 23:08:23 +0000

    By Hugo And Miguel
    ⚡🤔🙊🤨🥶🤠👽💩

---
+++ Jogo do Galo Hugo e Miguel/Form1.Designer.vb
@@ -0,0 +1,286 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox1.Cursor = System.Windows.Forms.Cursors.WaitCursor
+        Me.PictureBox1.Enabled = False
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(140, 244)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(94, 71)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.UseWaitCursor = True
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.PictureBox2.BackgroundImage = CType(resources.GetObject("PictureBox2.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox2.Cursor = System.Windows.Forms.Cursors.WaitCursor
+        Me.PictureBox2.Enabled = False
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(241, 244)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(94, 71)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        Me.PictureBox2.UseWaitCursor = True
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.PictureBox3.BackgroundImage = CType(resources.GetObject("PictureBox3.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox3.Cursor = System.Windows.Forms.Cursors.WaitCursor
+        Me.PictureBox3.Enabled = False
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(342, 244)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(94, 71)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        Me.PictureBox3.UseWaitCursor = True
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.PictureBox4.BackgroundImage = CType(resources.GetObject("PictureBox4.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox4.Cursor = System.Windows.Forms.Cursors.WaitCursor
+        Me.PictureBox4.Enabled = False
+        Me.PictureBox4.Image = CType(resources.GetObject("PictureBox4.Image"), System.Drawing.Image)
+        Me.PictureBox4.Location = New System.Drawing.Point(140, 320)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(94, 71)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 3
+        Me.PictureBox4.TabStop = False
+        Me.PictureBox4.UseWaitCursor = True
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.PictureBox5.BackgroundImage = CType(resources.GetObject("PictureBox5.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox5.Cursor = System.Windows.Forms.Cursors.WaitCursor
+        Me.PictureBox5.Enabled = False
+        Me.PictureBox5.Image = CType(resources.GetObject("PictureBox5.Image"), System.Drawing.Image)
+        Me.PictureBox5.Location = New System.Drawing.Point(241, 320)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(94, 71)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox5.TabIndex = 4
+        Me.PictureBox5.TabStop = False
+        Me.PictureBox5.UseWaitCursor = True
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.PictureBox6.BackgroundImage = CType(resources.GetObject("PictureBox6.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox6.Cursor = System.Windows.Forms.Cursors.WaitCursor
+        Me.PictureBox6.Enabled = False
+        Me.PictureBox6.Image = CType(resources.GetObject("PictureBox6.Image"), System.Drawing.Image)
+        Me.PictureBox6.Location = New System.Drawing.Point(342, 320)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(94, 71)
+        Me.PictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox6.TabIndex = 5
+        Me.PictureBox6.TabStop = False
+        Me.PictureBox6.UseWaitCursor = True
+        '
+        'PictureBox7
+        '
+        Me.PictureBox7.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.PictureBox7.BackgroundImage = CType(resources.GetObject("PictureBox7.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox7.Cursor = System.Windows.Forms.Cursors.WaitCursor
+        Me.PictureBox7.Enabled = False
+        Me.PictureBox7.Image = CType(resources.GetObject("PictureBox7.Image"), System.Drawing.Image)
+        Me.PictureBox7.Location = New System.Drawing.Point(140, 398)
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.Size = New System.Drawing.Size(94, 71)
+        Me.PictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox7.TabIndex = 6
+        Me.PictureBox7.TabStop = False
+        Me.PictureBox7.UseWaitCursor = True
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.PictureBox8.BackgroundImage = CType(resources.GetObject("PictureBox8.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox8.Cursor = System.Windows.Forms.Cursors.WaitCursor
+        Me.PictureBox8.Enabled = False
+        Me.PictureBox8.Image = CType(resources.GetObject("PictureBox8.Image"), System.Drawing.Image)
+        Me.PictureBox8.Location = New System.Drawing.Point(241, 398)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(94, 71)
+        Me.PictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox8.TabIndex = 7
+        Me.PictureBox8.TabStop = False
+        Me.PictureBox8.UseWaitCursor = True
+        '
+        'PictureBox9
+        '
+        Me.PictureBox9.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.PictureBox9.BackgroundImage = CType(resources.GetObject("PictureBox9.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox9.Cursor = System.Windows.Forms.Cursors.WaitCursor
+        Me.PictureBox9.Enabled = False
+        Me.PictureBox9.Image = CType(resources.GetObject("PictureBox9.Image"), System.Drawing.Image)
+        Me.PictureBox9.Location = New System.Drawing.Point(342, 398)
+        Me.PictureBox9.Name = "PictureBox9"
+        Me.PictureBox9.Size = New System.Drawing.Size(94, 71)
+        Me.PictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox9.TabIndex = 8
+        Me.PictureBox9.TabStop = False
+        Me.PictureBox9.UseWaitCursor = True
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button1.Font = New System.Drawing.Font("Microsoft YaHei UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(31, 26)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(109, 39)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Matura MT Script Capitals", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(273, 64)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(288, 89)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "é a tua vez :"
+        Me.Label1.Visible = False
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Cursor = System.Windows.Forms.Cursors.AppStarting
+        Me.Label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Label2.Font = New System.Drawing.Font("MV Boli", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(552, 64)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(342, 210)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Label2"
+        Me.Label2.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(950, 506)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox9)
+        Me.Controls.Add(Me.PictureBox8)
+        Me.Controls.Add(Me.PictureBox7)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Jogo Do Galo"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents PictureBox7 As PictureBox
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents PictureBox9 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Jogo do Galo Hugo e Miguel/Form1.vb
@@ -0,0 +1,263 @@
+﻿Public Class Form1
+    Dim jogador1, jogador2, vencedor As String
+    Dim vez As Integer
+    Dim mg1, mg2 As Image
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        jogador1 = InputBox("Insira o seu nome..... Você será o X", "Olá Jogador 1 ;)")
+        jogador2 = InputBox("Insira o seu nome..... Você será a O", "Olá Jogador 2 ;)")
+        Button1.Visible = False
+        Label1.Visible = True
+        Label2.Visible = True
+        Label2.Text = jogador1
+
+
+        PictureBox1.Enabled = True
+        PictureBox2.Enabled = True
+        PictureBox3.Enabled = True
+        PictureBox4.Enabled = True
+        PictureBox5.Enabled = True
+        PictureBox6.Enabled = True
+        PictureBox7.Enabled = True
+        PictureBox8.Enabled = True
+        PictureBox9.Enabled = True
+
+    End Sub
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+        vez = vez + 1
+        PictureBox1.Enabled = False
+        If vez Mod 2 = 0 Then
+            Label2.Text = jogador1
+        Else
+            Label2.Text = jogador2
+        End If
+
+
+        If vez Mod 2 = 0 Then
+            PictureBox1.Image = mg2
+        Else
+            PictureBox1.Image = mg1
+
+        End If
+
+        Verificar(jogador1, jogador2)
+
+    End Sub
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+
+        vez = vez + 1
+        PictureBox2.Enabled = False
+        If vez Mod 2 = 0 Then
+            Label2.Text = jogador1
+        Else
+            Label2.Text = jogador2
+        End If
+
+        If vez Mod 2 = 0 Then
+            PictureBox2.Image = mg2
+        Else
+            PictureBox2.Image = mg1
+
+        End If
+
+        Verificar(jogador1, jogador2)
+
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+
+        vez = vez + 1
+        PictureBox3.Enabled = False
+        If vez Mod 2 = 0 Then
+            Label2.Text = jogador1
+        Else
+            Label2.Text = jogador2
+        End If
+
+        If vez Mod 2 = 0 Then
+            PictureBox3.Image = mg2
+        Else
+            PictureBox3.Image = mg1
+        End If
+
+        Verificar(jogador1, jogador2)
+
+    End Sub
+
+    Private Sub PictureBox4_Click(sender As Object, e As EventArgs) Handles PictureBox4.Click
+
+        vez = vez + 1
+        PictureBox4.Enabled = False
+        If vez Mod 2 = 0 Then
+            Label2.Text = jogador1
+        Else
+            Label2.Text = jogador2
+        End If
+
+        If vez Mod 2 = 0 Then
+            PictureBox4.Image = mg2
+        Else
+            PictureBox4.Image = mg1
+
+        End If
+
+        Verificar(jogador1, jogador2)
+
+    End Sub
+
+    Private Sub PictureBox5_Click(sender As Object, e As EventArgs) Handles PictureBox5.Click
+
+        vez = vez + 1
+        PictureBox5.Enabled = False
+        If vez Mod 2 = 0 Then
+            Label2.Text = jogador1
+        Else
+            Label2.Text = jogador2
+        End If
+
+        If vez Mod 2 = 0 Then
+            PictureBox5.Image = mg2
+        Else
+            PictureBox5.Image = mg1
+
+        End If
+
+        Verificar(jogador1, jogador2)
+
+    End Sub
+
+    Private Sub PictureBox6_Click(sender As Object, e As EventArgs) Handles PictureBox6.Click
+
+        vez = vez + 1
+        PictureBox6.Enabled = False
+        If vez Mod 2 = 0 Then
+            Label2.Text = jogador1
+        Else
+            Label2.Text = jogador2
+        End If
+
+        If vez Mod 2 = 0 Then
+            PictureBox6.Image = mg2
+        Else
+            PictureBox6.Image = mg1
+
+        End If
+
+        Verificar(jogador1, jogador2)
+
+    End Sub
+
+    Private Sub PictureBox7_Click(sender As Object, e As EventArgs) Handles PictureBox7.Click
+
+        vez = vez + 1
+        PictureBox7.Enabled = False
+        If vez Mod 2 = 0 Then
+            Label2.Text = jogador1
+        Else
+            Label2.Text = jogador2
+        End If
+
+        If vez Mod 2 = 0 Then
+            PictureBox7.Image = mg2
+        Else
+            PictureBox7.Image = mg1
+
+        End If
+
+        Verificar(jogador1, jogador2)
+    End Sub
+
+    Private Sub PictureBox8_Click(sender As Object, e As EventArgs) Handles PictureBox8.Click
+
+        vez = vez + 1
+        PictureBox8.Enabled = False
+        If vez Mod 2 = 0 Then
+            Label2.Text = jogador1
+        Else
+            Label2.Text = jogador2
+
+        End If
+
+        If vez Mod 2 = 0 Then
+            PictureBox8.Image = mg2
+        Else
+            PictureBox8.Image = mg1
+
+        End If
+
+        Verificar(jogador1, jogador2)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If vez Mod 2 = 0 Then
+            Label2.Text = jogador1
+        Else
+            Label2.Text = jogador2
+        End If
+
+        mg1 = My.Resources.Resource1.X_1
+
+        mg2 = My.Resources.Resource1.O_2
+    End Sub
+
+    Private Sub PictureBox9_Click(sender As Object, e As EventArgs) Handles PictureBox9.Click
+
+        vez = vez + 1
+        PictureBox9.Enabled = False
+        If vez Mod 2 = 0 Then
+            Label2.Text = jogador1
+        Else
+            Label2.Text = jogador2
+        End If
+
+        If vez Mod 2 = 0 Then
+            PictureBox9.Image = mg2
+        Else
+            PictureBox9.Image = mg1
+
+        End If
+        Verificar(jogador1, jogador2)
+    End Sub
+
+
+    Sub Verificar(ByVal jogador1 As String, ByVal Jogador2 As String)
+
+        If (vez + 1) Mod 2 = 0 Then
+            vencedor = jogador1
+        Else
+            vencedor = Jogador2
+        End If
+
+
+        If (PictureBox1.Image.Equals(PictureBox2.Image) = True) And (PictureBox2.Image.Equals(PictureBox3.Image) = True) Then
+            MsgBox("Parabens " & vencedor & " você ganhou")
+
+        ElseIf (PictureBox4.Image.Equals(PictureBox5.Image) = True) And (PictureBox5.Image.Equals(PictureBox6.Image) = True) Then
+            MsgBox("Parabens " & vencedor & " você ganhou")
+
+        ElseIf (PictureBox7.Image.Equals(PictureBox8.Image) = True) And (PictureBox8.Image.Equals(PictureBox9.Image) = True) Then
+            MsgBox("Parabens " & vencedor & " você ganhou")
+
+        ElseIf (PictureBox1.Image.Equals(PictureBox4.Image) = True) And (PictureBox4.Image.Equals(PictureBox7.Image) = True) Then
+            MsgBox("Parabens " & vencedor & " você ganhou")
+
+        ElseIf (PictureBox2.Image.Equals(PictureBox5.Image) = True) And (PictureBox5.Image.Equals(PictureBox8.Image) = True) Then
+            MsgBox("Parabens " & vencedor & " você ganhou")
+
+        ElseIf (PictureBox3.Image.Equals(PictureBox6.Image) = True) And (PictureBox6.Image.Equals(PictureBox9.Image) = True) Then
+            MsgBox("Parabens " & vencedor & " você ganhou")
+
+        ElseIf (PictureBox1.Image.Equals(PictureBox5.Image) = True) And (PictureBox5.Image.Equals(PictureBox9.Image) = True) Then
+            MsgBox("Parabens " & vencedor & " você ganhou")
+
+        ElseIf (PictureBox3.Image.Equals(PictureBox5.Image) = True) And (PictureBox5.Image.Equals(PictureBox7.Image) = True) Then
+            MsgBox("Parabens " & vencedor & " você ganhou")
+
+        ElseIf vez > 8 Then
+            MsgBox(" Empate ")
+        End If
+
+    End Sub
+End Class
